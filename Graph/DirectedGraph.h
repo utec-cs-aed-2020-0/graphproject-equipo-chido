92ba48ebb2a73fd0acc6379e7088364e113cc639
Claudia <claudia.pacori@utec.edu.pe>
2022-02-18 11:37:38 -0500

    metodos para dirigido y no dirigido

--- Graph/DirectedGraph.h
+++ Graph/DirectedGraph.h
@@ -6,6 +6,83 @@
 template<typename TV, typename TE>
 class DirectedGraph : public Graph<TV, TE>{
 
+public:
+    DirectedGraph(){};
+    ~DirectedGraph() = default;
+
+    bool insertVertex(string id, TV vertex){throw("missing");}
+
+    bool createEdge(string id1, string id2, TE w){
+        // Si existen los vertices agrega la arista
+        if (findById(id1) && findById(id2)) {
+            Edge<TV, TE>* edgecreado = new Edge<TV, TE>(this->vertexes[id1],this->vertexes[id2],w));
+            this->vertexes[id1]->edges.push_back(edgecreado);
+            return true;
+        }
+        return false;
+    }
+
+    bool deleteVertex(string id){
+        if (findById(id)){
+            //Si existe el vertice elimina las aristas que apunten a este
+            for (auto it = this->vertexes.begin(); it != this->vertexes.end(); it++) {
+                if (it->first != id)
+                    deleteEdge(it->first, id);
+            }
+            this->vertexes.erase(this->vertexes.find(id));
+            return true;
+        }
+        return false;
+    }
+
+    bool deleteEdge(string id){throw("missing");}
+
+    TE &operator()(string start, string end){
+        if (findById(start) && findById(end)) {
+            //Si existen los vertices start y end
+            for (auto edge : this->vertexes[start]->edges) {
+                //Se busca la arista que los una y retorna el peso
+                if (edge->vertexes[0] == this->vertexes[start] && edge->vertexes[1] == this->vertexes[end])
+                    return edge->weight;
+            }
+        }
+        throw("no existe");
+    }
+
+    float density(){
+        float v, e;
+        //contar vertices y aristas
+        v = this->vertexes.size();
+        e = 0;
+        for (auto it = this->vertexes.begin(); it != this->vertexes.end(); it++) {
+            e += it->second->edges.size();
+        }
+        //formula para un grafo dirigido
+        return e/(v*(v-1));
+    }
+
+    bool isDense(float threshold = 0.5){throw("missing");}
+
+    bool isConnected(){throw("missing");}
+
+    bool isStronglyConnected(){throw("missing");}
+
+    bool empty(){
+        return (this->vertexes.size()==0;)
+    }
+
+    void clear(){
+        this->vertexes.clear();
+    }
+      
+    void displayVertex(string id){throw("missing");}
+
+    bool findById(string id){
+        return (this->vertexes.find(id) != this->vertexes.end());
+    }
+
+    void display(){throw("missing");}
+
 };
 
 #endif
--- Graph/graph.h
+++ Graph/graph.h
@@ -22,35 +22,53 @@ template<typename TV, typename TE>
 struct Edge {
     Vertex<TV, TE>* vertexes[2];
     TE weight;
+
+    Edge(){
+      this->vertexes[0]=nullptr;
+      this->vertexes[1]=nullptr;
+      this->weight=0;
+    }
+    Edge(Vertex<TV,TE>* Start, Vertex<TV,TE>* End,TE peso){
+      this->vertexes[0]=Start;
+      this->vertexes[1]=End;
+      this->weight=peso;
+    }
+
 };
 
 template<typename TV, typename TE>
 struct Vertex {
     TV data;
     std::list<Edge<TV, TE>*> edges;
+
+    Vertex(){
+      this->data=0;
+    };
+    Vertex(TV dato){
+      this->data=dato;
+    }
 };
 
 template<typename TV, typename TE>
 class Graph{
-private:    
-    std::unordered_map<string, Vertex<TV, TE>*>  vertexes;
-    
+
 public:
-    bool insertVertex(string id, TV vertex) = 0;   
-    bool createEdge(string id1, string id2, TE w) = 0;     
-    bool deleteVertex(string id) = 0;     
-    bool deleteEdge(string id) = 0;   
-    TE &operator()(string start, string end)= 0;  
-    float density() = 0;
-    bool isDense(float threshold = 0.5) = 0;
-    bool isConnected()= 0;
-    bool isStronglyConnected() throw();
-    bool empty();
-    void clear()= 0;  
+    std::unordered_map<string, Vertex<TV, TE>*>  vertexes;
+    virtual bool insertVertex(string id, TV vertex) = 0;   
+    virtual bool createEdge(string id1, string id2, TE w) = 0;     
+    virtual bool deleteVertex(string id) = 0;     
+    virtual bool deleteEdge(string id) = 0;   
+    virtual TE &operator()(string start, string end)= 0;  
+    virtual float density() = 0;
+    virtual bool isDense(float threshold = 0.5) = 0;
+    virtual bool isConnected()= 0;
+    virtual bool isStronglyConnected() throw();
+    virtual bool empty();
+    virtual void clear()= 0;  
       
-    void displayVertex(string id)= 0;
-    bool findById(string id) = 0;
-    void display() = 0;
+    virtual void displayVertex(string id)= 0;
+    virtual bool findById(string id) = 0;
+    virtual void display() = 0;
 };
 
 #endif
