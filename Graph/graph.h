f737c35d86041289750eec36a0ea87fdcffb2601
Claudia-Pacori <55209248+Claudia-Pacori@users.noreply.github.com>
2022-02-22 23:25:31 -0500

    completo

--- Graph/graph.h
+++ Graph/graph.h
@@ -20,7 +20,7 @@ class Graph;
 
 template<typename TV, typename TE>
 struct Edge {
-    Vertex<TV, TE>* vertexes[2];
+    Vertex<TV,TE>* vertexes[2];
     TE weight;
 
     Edge(){
@@ -38,14 +38,16 @@ struct Edge {
 
 template<typename TV, typename TE>
 struct Vertex {
+    string id;  
     TV data;
     std::list<Edge<TV, TE>*> edges;
 
     Vertex(){
       this->data=0;
     };
-    Vertex(TV dato){
-      this->data=dato;
+    Vertex(TV dato, string id){
+      this -> data=dato;
+      this -> id = id; 
     }
 };
 
@@ -57,15 +59,14 @@ public:
     virtual bool insertVertex(string id, TV vertex) = 0;   
     virtual bool createEdge(string id1, string id2, TE w) = 0;     
     virtual bool deleteVertex(string id) = 0;     
-    virtual bool deleteEdge(string id) = 0;   
+    virtual bool deleteEdge(string id_inicio, string id_final) = 0;   
     virtual TE &operator()(string start, string end)= 0;  
     virtual float density() = 0;
     virtual bool isDense(float threshold = 0.5) = 0;
     virtual bool isConnected()= 0;
-    virtual bool isStronglyConnected() throw();
-    virtual bool empty();
+    virtual bool isStronglyConnected() = 0;
+    virtual bool empty() = 0;
     virtual void clear()= 0;  
-      
     virtual void displayVertex(string id)= 0;
     virtual bool findById(string id) = 0;
     virtual void display() = 0;
