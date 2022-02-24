b0140a95fb2c056bd3fefd0c250214e2603424ce
Claudia-Pacori <55209248+Claudia-Pacori@users.noreply.github.com>
2022-02-23 23:38:28 -0500

    bfs modificado

--- Graph/Algorithms/bfs.h
+++ Graph/Algorithms/bfs.h
@@ -2,76 +2,54 @@
 #define BFS_H
 
 #include "../graph.h"
-#include <iostream>
 #include <queue>
 #include <unordered_map>
 
+using namespace std;
+
 template<typename TV, typename TE>
+class BFS {
+private:
+	DirectedGraph<TV, TE>* BFSapplied = new DirectedGraph<TV,TE>() ;
+public:
+	BFS(DirectedGraph<TV, TE>* graph, string start) {
+		unordered_map<TV, string> help;
+		for (auto it = graph->vertexes.begin(); it != graph->vertexes.end(); it++) {
+            help[it->second->data] = it->first;
+		}
 
-class BFS{
-    
-    private:
-        Graph<TV,TE>* graph;
-        string id_inicial;
-        vector<Vertex<TV, TE> *> BFSapplied;
+		std::unordered_map<TV, TV> visited;
+		queue<TV> resQ;
+		TV aux = graph->vertexes.find(start)->second->data;
+		resQ.push(aux);
+		visited[aux] = aux;
+		string auxS = help.find(aux)->second;
+		this->BFSapplied->insertVertex(auxS, aux);
+		TV aux2 = aux;
+		while (!resQ.empty()) {
+			aux = resQ.front();
+			resQ.pop();
+			auxS = help.find(aux)->second;
+			for (auto it2 = graph->vertexes.find(auxS)->second->edges.begin(); it2 != graph->vertexes.find(auxS)->second->edges.end(); it2++) {
+				aux2 = (*it2)->vertexes[1]->data;
+                //cout<<"analizando: "<<auxS<<endl;
+				if (visited.find(aux2) == visited.end()) {
+					visited[aux2] = aux2;
+					resQ.push(aux2);
+					this->BFSapplied->insertVertex(help.find(aux2)->second, aux2);
+					this->BFSapplied->createEdge(auxS, help.find(aux2)->second, (*it2)->weight);
+                    //cout<<"conectando con: "<<(*it2)->vertexes[1]->id<<endl;
+				}
+			}
+		}
+	}
 
-
-    public:
-        BFS(Graph<TV,TE>* _graph, string _id_inicial) : graph(_graph), id_inicial(_id_inicial) {
-            BFSapplied = apply();
-        }; 
-        ~BFS() = default;
-
-        vector<Vertex<TV, TE> *> apply() {
-            if (!this->graph->isConnected()) {
-            throw("BFS no se puede aplicar");
-            }
-            vector<Vertex<TV, TE> *> result;
-
-            unordered_map<Vertex<TV, TE> *, bool> visited;
-            auto it = this->graph->vertexes.begin();
-            while (it != this->graph->vertexes.end()) {
-            visited[it->second] = false;
-            it++;
-            }
-
-            std::list<Vertex<TV, TE> *> queue;
-            visited[this->graph->vertexes[this->id_inicial]] = true;
-
-            queue.push_back(this->graph->vertexes[this->id_inicial]);
-
-            while (!queue.empty()) {
-            Vertex<TV, TE> *s = queue.front();
-            result.push_back(s);
-            queue.pop_front();
-
-            for (auto it = s->edges.begin(); it != s->edges.end(); ++it) {
-                if (!visited[(*it)->vertexes[0]]) {
-                visited[(*it)->vertexes[0]] = true;
-                queue.push_back((*it)->vertexes[0]);
-                }
-                if (!visited[(*it)->vertexes[1]]) {
-                visited[(*it)->vertexes[1]] = true;
-                queue.push_back((*it)->vertexes[1]);
-                }
-            }
-            }
-            return result;
-        }
+	~BFS() = default;
+	Graph<TV,TE>* get_graph(){return BFSapplied;}
 
         void display(){
-            cout<<"Unido desde: "<<id_inicial<<" -> ";
-            for(int i=0; i<BFSapplied.size();i++){
-                cout<<BFSapplied[i]->id<<" ("<<BFSapplied[i]->data<<") ";
-            }
-            cout<<endl;
+            BFSapplied -> display();
         }
-
-
-
 };
-
-
-
 
 #endif
